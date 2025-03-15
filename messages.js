c10b91fc95c68e86d070427b605d78ae6432f0e2
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-03-15 07:55:47 +0200

    style: format message keys for improved readability

--- messages.js
+++ messages.js
@@ -1,28 +1,29 @@
 const messageKeys = [
-    "welcomeTitle",
-    "welcomeHeading",
-    "welcomeMessage",
-    "GoToChatGpt",
-    "whatsNewTitle",
-    "whatsNewMessage",
-    "featuresTitle",
-    "improvementsTitle",
-    "feature1",
-    "feature2",
-    "feature3",
-    "feature3Comment",
-    "feature4",
-    "improvement1",
-    "improvement2",
-    "comingSoonTitle",
-    "comingSoonMessage",
-    "comingSoonFeature1",
-    "feedbackTitle",
-    "feedbackMessage",
-    "feedbackButtonText",
+  "welcomeTitle",
+  "welcomeHeading",
+  "welcomeMessage",
+  "GoToChatGpt",
+  "whatsNewTitle",
+  "whatsNewMessage",
+  "featuresTitle",
+  "improvementsTitle",
+  "feature1",
+  "feature2",
+  "feature3",
+  "feature3Comment",
+  "feature4",
+  "improvement1",
+  "improvement2",
+  "comingSoonTitle",
+  "comingSoonMessage",
+  "comingSoonFeature1",
+  "feedbackTitle",
+  "feedbackMessage",
+  "feedbackButtonText",
 ];
+
 const messages = messageKeys.reduce((acc, key) => {
-    acc[key] = chrome.i18n.getMessage(key);
-    return acc;
+  acc[key] = chrome.i18n.getMessage(key);
+  return acc;
 }, {});
 export default messages;
