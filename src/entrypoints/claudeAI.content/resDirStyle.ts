a95f5851ef76014ee752b5368b58d6b8ceea5879
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-05-06 19:26:44 +0300

    fix: fix math expression like (1/cos) to support text direction

--- src/entrypoints/claudeAI.content/resDirStyle.ts
+++ src/entrypoints/claudeAI.content/resDirStyle.ts
@@ -1,3 +1,32 @@
+function wrapMathExpressions(root: HTMLElement): void {
+  const walker: TreeWalker = document.createTreeWalker(
+    root,
+    NodeFilter.SHOW_TEXT,
+    null
+  );
+
+  const mathRegex: RegExp = /\((\d+\/[a-zA-Z]+)\)/g;
+
+  let currentNode: Node | null;
+  while ((currentNode = walker.nextNode())) {
+    const textNode = currentNode as Text;
+    const originalText = textNode.nodeValue;
+
+    if (originalText && mathRegex.test(originalText)) {
+      const span: HTMLSpanElement = document.createElement("span");
+      span.innerHTML = originalText.replace(
+        mathRegex,
+        `<span style="direction:ltr; unicode-bidi:isolate">($1)</span>`
+      );
+
+      const parent = textNode.parentNode;
+      if (parent) {
+        parent.replaceChild(span, textNode);
+      }
+    }
+  }
+}
+
 function handleMathTextDirection(mathElement: HTMLElement) {
   mathElement.style.setProperty("unicode-bidi", "plaintext");
   mathElement.style.setProperty("direction", "ltr");
@@ -13,11 +42,23 @@ export function applyRTLStyleToGptResponse(gptResponseEl: HTMLDivElement) {
     "p.whitespace-pre-wrap.break-words, li.whitespace-normal.break-words, div.math.math-display, h3, h4"
   );
 
-  elementsContainingMath.forEach((el) => {
-    el.style.setProperty("unicode-bidi", "plaintext");
+  // An example of a math expression: (1/cos)
+  const mathRegex = /\((\d+\/[a-zA-Z]+)\)/g;
+
+  for (const el of elementsContainingMath) {
+    console.log("Math element found:", el);
+    const html = el.innerHTML;
+
+  if (mathRegex.test(html)) {
+    el.innerHTML = html.replace(
+      mathRegex,
+      '<span style="direction:ltr; unicode-bidi:isolate">($1)</span>'
+    );
+
     el.style.lineHeight = "2";
     el.style.marginBlock = "2px";
-  });
+  }
+  };
 
   // Set the text direction for all dynamic paragraphs
   for (const el of elementsContainingMath) {
